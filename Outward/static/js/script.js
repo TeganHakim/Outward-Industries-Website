3472023887d025599e92f043a1b57edb241a1cc9
TeganHakim <67878686+TeganHakim@users.noreply.github.com>
2023-02-17 21:33:05 -0500

    Implementation of site

--- Outward/static/js/script.js
+++ Outward/static/js/script.js
@@ -1,12 +1,3 @@
-/*!
-* Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 window.addEventListener('DOMContentLoaded', event => {
 
   // Navbar shrink function
@@ -52,3 +43,5 @@ window.addEventListener('DOMContentLoaded', event => {
   });
 
 });
+
+document.getElementById('copy-date').innerText = new Date().getFullYear();
