2829851b627b795d15fc80f6593566c1d3f4805b
TeganHakim <67878686+TeganHakim@users.noreply.github.com>
2023-02-20 18:57:28 -0500

    site work more

--- Outward/urls.py
+++ Outward/urls.py
@@ -6,4 +6,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('terms-of-use/', views.terms_of_use, name="terms-of-use"),
+    path('privacy-policy/', views.privacy_policy, name="privacy-policy"),
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
--- Outward/views.py
+++ Outward/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "index.html");
+
+def terms_of_use(request):
+    return render(request, "terms-of-use.html");
+    
+def privacy_policy(request):
+    return render(request, "privacy-policy.html");
