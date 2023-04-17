b7a989cf13d99edd6eb58ef7f4f97fff60658f92
TeganHakim <67878686+TeganHakim@users.noreply.github.com>
2023-04-17 00:01:56 -0400

    Almost done... corrections and edits

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,16 +1,27 @@
 function loader() {
-    window.scrollTo(0, 0);
-    document.body.style = "overflow: hidden;";
-    var loader = document.querySelector(".loader");
-    loader.style.opacity = 1;
-    loader.style.display = "flex";
-    setTimeout(() => {
-      $(".loader-span").fadeOut(2000);
-    }, 1100);
-    setTimeout(() => {
+  window.scrollTo(0, 0);
+  if (window.innerWidth < 768) {
+  setTimeout(() => {
+      $(".loader-span").fadeOut(1000);
+      document.body.style = "overflow-y: scroll;";
+  }, 1000);
+  setTimeout(() => {
       $(".loader").fadeOut(1000);
       document.body.style = "overflow-y: scroll;";
-    }, 1800);
+  }, 1700);
+  } else {
+      document.body.style = "overflow: hidden;";
+      var loader = document.querySelector(".loader");
+      loader.style.opacity = 1;
+      loader.style.display = "flex";
+      setTimeout(() => {
+        $(".loader-span").fadeOut(2000);
+      }, 1100);
+      setTimeout(() => {
+        $(".loader").fadeOut(1000);
+        document.body.style = "overflow-y: scroll;";
+      }, 1800);
+  }
 }
 
 window.addEventListener("load", () => {
